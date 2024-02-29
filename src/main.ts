0031aaa30b0993fcd46aea566f7a36c8313423dc
Nick Vigier <nvigier46@gmail.com>
2024-02-29 08:47:37 +0000

    Trigger build from commit 2

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 // import { AppModule } from './app.module';
 
-console.log(1);
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
